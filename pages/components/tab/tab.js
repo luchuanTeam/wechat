b99d6e954f554ab189a31469f6ea641a54607818
melvzz <277514786@qq.com>
2018-02-08 14:51:00 +0800

    '新增组件'

---
+++ pages/components/tab/tab.js
@@ -0,0 +1,17 @@
+Component({
+  properties: {
+    activeTab: {
+      type: Number,
+      value: 0
+    }
+  },
+  data: {
+    menus: ['注册', '登陆', '扫一扫', '升级VIP', '推荐']
+  },
+  methods: {
+    menuTap(e) {
+      let index = e.currentTarget.dataset.index; //获取点击菜单的下标
+      this.triggerEvent('tabchange', {index:index});
+    }
+  }
+})
