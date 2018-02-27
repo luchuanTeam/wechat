68535958f016760662e2ae1ab7433e7c1ebb676a
melvzz <277514786@qq.com>
2018-02-27 10:34:17 +0800

    '增加分区'

--- pages/content/content.js
+++ pages/content/content.js
@@ -1,39 +1,17 @@
-
-const POEM = 0,   //唐诗
-      MANNER = 1, //日常礼仪
-      CHILDRENL = 2,  // 幼儿语文 
-      PRIMARYL = 3,   // 小学语文
-      MIDDLEL = 4;    // 中学语文
-let _details = {
-  title: '唐诗',
-  data: [
-    {
-      imgUrl: 'http://www.yanda123.com/images/1518273153583.jpg',
-      title: '我是陈大牛',
-      introduce: '我是陈立，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈'
-    },
-    {
-      imgUrl: 'http://www.yanda123.com/images/1518273153583.jpg',
-      title: '我是陈大牛',
-      introduce: '我是陈立，还有谁?'
-    },
-    {
-      imgUrl: 'http://www.yanda123.com/images/1518273153583.jpg',
-      title: '我是陈大牛',
-      introduce: '我是陈立，、还有谁?'
-    },
-    {
-      imgUrl: 'http://www.yanda123.com/images/1518273153583.jpg',
-      title: '我是陈大牛',
-      introduce: '我是陈立，人的还有谁?'
-    }
+const stations = [
+  [
+    {imgUrl: '../../../resources/images/chuzhong.png', desc: '初中'},
+    {imgUrl: '../../../resources/images/chuzhong.png', desc: '初中'},
+    {imgUrl: '../../../resources/images/chuzhong.png', desc: '初中'},
+    {imgUrl: '../../../resources/images/chuzhong.png', desc: '初中'}
+  ],
+  [
+    { imgUrl: '../../../resources/images/chuzhong.png', desc: '幼教(4-6岁)' },
+    { imgUrl: '../../../resources/images/chuzhong.png', desc: '初中' },
+    { imgUrl: '../../../resources/images/chuzhong.png', desc: '初中' },
+    { imgUrl: '../../../resources/images/chuzhong.png', desc: '初中' }
   ]
-};      
-let obj = {
-  imgUrl: 'http://www.yanda123.com/images/1518273153583.jpg',
-  title: '我是陈大牛',
-  introduce: '我是陈立，、还有谁?'  
-};
+]
 
 
 Page({
@@ -42,54 +20,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activeTab: POEM,
-    title: "主页",
-    imgUrls: [],
-    details: {    // 详细界面的数据
-      title: '',    // 标题
-      data: [    // 具体数据
-        {
-          imgUrl: '',   //图片地址
-          title: '',   //标题
-          introduce: ''   //介绍
-        }
-      ]   
-    },
-    loadingShow: '1',
-    getNewData: 0
-  },
-
-  activeTabChange(e) {
-    
-    this.setData({
-      activeTab: e.detail.index,
-      title: "主页君，你好"
-    });
-    /* 以下方法应为调用后端接口， 根据点击的菜单栏传对应的参数值，获取相对应的数据 */
-
-    if(e.detail.index === 0) {
-      this.setData({
-        details: {
-          title: '唐诗',
-          data: _details.data
-        }
-      })
-    } else if(e.detail.index === 1) {
-      this.setData({
-        details: {
-          title: '日常礼仪',
-          data: _details.data
-        }
-      });  
-    } else {
-      this.setData({
-        details: {
-          title: '其他',
-          data: _details.data
-        }
-      });    
-    }
-    
+    stations: stations
   },
 
   /**
@@ -119,11 +50,7 @@ Page({
         console.log(err)
       }
     });
-    /* 此处还应加一个方法，获取 唐诗 对应的数据 */
     
-    this.setData({
-      details: _details
-    })
   },
 
   /**
@@ -159,29 +86,6 @@ Page({
    */
   onPullDownRefresh: function (e) {
     console.dir(e); 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function (e) {
-    if(this.data.getNewData < 5) {
-      _details.data.push(obj);
-      setTimeout(() => {
-        this.data.getNewData++;
-        this.setData({
-          details: _details
-        });
-        if (this.data.getNewData >= 5){
-          this.setData({
-            loadingShow: '0'
-          })
-        }
-      }, 1000);
-      
-    } 
-    
-    
   },
 
   /**
