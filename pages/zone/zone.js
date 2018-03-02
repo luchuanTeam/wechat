6b30bcf8a535679b83756ee05bc248bfb0df411a
melvzz <277514786@qq.com>
2018-03-02 17:29:21 +0800

    分类页面设计

--- pages/zone/zone.js
+++ pages/zone/zone.js
@@ -1,11 +1,38 @@
-// pages/zone/zone.js
+
+const TOP_CATEGORY = ['胎教', '早教(1-3岁)', '幼教(4-6岁)', '小学课程', '初中课程', '高中课程', '国学', '手工课程'];
+const STYLE_ARR = ['baby-blue', 'pink', 'olive-green', 'orange', 'light-cyan'];
+const TESTARR1 = ['胎教音乐', '国学胎教(音频)'];
+const TESTARR2 = ['小学语文(1-6年级)', '小学数学(1-6年级)', '小学英语(1-6年级)'];
+const TESTARR3 = ['helloworld', 'helloworld', 'helloworld', 'helloworld', 'helloworld', 'helloworld', 'helloworld', 'helloworld']
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    topCategory: TOP_CATEGORY,
+    selected: 0,
+    styleArr: STYLE_ARR,
+    secondCategory: TESTARR1
+  },
+
+  /**
+   * 点击一级目录更改显示
+   */
+  changeSelected(e) {
+    let data = [];
+    if(e.target.dataset.index === 0){
+      data = TESTARR1;
+    } else if (e.target.dataset.index === 1) {
+      data = TESTARR2;
+    } else {
+      data = TESTARR3;
+    }
+    this.setData({
+      selected: e.target.dataset.index,
+      secondCategory: data
+    })
+    
   },
 
   /**
