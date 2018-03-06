701d7fe17b3f973c91f6d9c8c8267d2d2179793a
melvzz <277514786@qq.com>
2018-03-06 15:07:45 +0800

    视频页面的完善

--- pages/video/video.js
+++ pages/video/video.js
@@ -6,19 +6,25 @@ Page({
    */
   data: {
     showCenterPlayBtn: false,
-    selected: '1',
-    video: {
-      src: '',                           // 视频地址
-      title: '我是陈大牛',                // 视频标题
-      intro: {                           // 视频简介
-        show: '这是唐代诗人王维创作的一首劝慰友人落第的诗',                        // 进入页面就显示的简介
-        hide: 'helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld'                         // 点击简介后才显示的简介内容
+    selected: '1',        // 决定显示视频组件或者评论组件, '1'代表视频组件, '2'代表评论组件
+    videoData: {        // 视频组件的状态数据
+      video: {
+        src: '',                           // 视频地址
+        title: '我是陈大牛',                // 视频标题
+        intro: {                           // 视频简介
+          show: '这是唐代诗人王维创作的一首劝慰友人落第的诗',                        // 进入页面就显示的简介
+          hide: 'helloworld'                         // 点击简介后才显示的简介内容
+        },
+        series: '国学/唐诗系列',                 // 系列  
+        episodes: 20                          // 集数
       },
-      series: '国学/唐诗系列',                 // 系列  
-      episodes: 20
+      showVideoHiddenIntro: '0',  //  控制隐藏简介的显示状态, '1'代表显示
+      playing: 0,               // 正在播放的集数，控制绿色三角形的显示
+      showHideEpisode: '0'      // 控制全部集数的组件的显示状态, '1'代表显示
     },
-    showVideoHiddenIntro: '0',
-    playing: 0
+    commentData: {
+      selected: '1'           // 评论的展示类型 1所有 2最新 3精华
+    }
   },
 
   /**
@@ -30,19 +36,42 @@ Page({
     });
   },
 
+  /**
+   * 点击改变隐藏简介显示的状态
+   */
   toggleHideIntro() {
+    let data = 'videoData.showVideoHiddenIntro';
     this.setData({
-      showVideoHiddenIntro: this.data.showVideoHiddenIntro === '0'?'1':'0' 
-    })
+      [data]: this.data.videoData.showVideoHiddenIntro === '0'?'1':'0' 
+    });
   },
 
   /**
    * 点击更换播放的集数
    */
   togglePlay(e) {
+    let data = 'videoData.playing';
     this.setData({
-      playing: e.currentTarget.dataset.index   
-    })
+      [data]: e.currentTarget.dataset.index   
+    });
+  },
+  /**
+   * 点击改变 全部集数的组件 的显示状态
+   */
+  toggleHideEpisode(e) {
+    let data = 'videoData.showHideEpisode';
+    this.setData({
+      [data]: this.data.videoData.showHideEpisode === '0' ? '1' : '0' 
+    });
+  },
+  /**
+   * 点击改变 评论 的过滤条件
+   */
+  toggleCommentFilter(e) {
+    let data = 'commentData.selected';
+    this.setData({
+      [data]: e.currentTarget.dataset.index
+    });
   },
 
   /**
