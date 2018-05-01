163b0b7c74b9db190b09f7040c56eaa2281987e9
chenli <chen.li@trs.com.cn>
2018-05-01 22:38:34 +0800

    登录方法调整

--- pages/user/home/home.js
+++ pages/user/home/home.js
@@ -42,30 +42,80 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse) {
+      var that = this;
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        });
+        that.initUserInfo(res.userInfo);
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
+      var that = this;
       wx.getUserInfo({
-        withCredentials: true,
         success: res => {
-          app.globalData.userInfo = res.userInfo;
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+          that.initUserInfo(res.userInfo);
         }
       })
     }
   },
   getUserInfo: function (e) {
     let userInfo = e.detail.userInfo;
+    this.initUserInfo(userInfo);
+  },
+  /**
+   * 通过微信用户信息在后台注册随机账号，并与微信做绑定
+   */
+  register: function (openId, nickName, avatar, gender) {
+    $.post({
+      url: 'https://www.yanda123.com/yanda/user/registerByWechat',
+      header: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: {
+        openId: openId,
+        nickName: nickName,
+        avatar: avatar,
+        sex: gender
+      }
+    }).then((res) => {
+      let data = res.data;
+      if (data.status == 200) {
+        let userInfo = data.data;
+        console.log(userInfo);
+        this.login(userInfo.userName, userInfo.passowrd);
+      } else {
+        console.log('注册微信用户失败:' + data.message);
+      }
+    })
+  },
+  /**
+   * 在后台进行登录
+   */
+  login: function (userName, password) {
+    $.post({
+      url: 'https://www.yanda123.com/yanda/user/login',
+      header: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: {
+        userName: userName,
+        password: password
+      }
+    }).then((res) => {
+      let result = res.data;
+      if (result.status === -1) {
+        utils.quickTip(result.message);
+      } else if (result.status === 200) {
+        wx.setStorageSync('userInfo', result.data.userInfo);
+        wx.setStorageSync('sessionId', result.data.sessionId);
+        wx.setStorageSync('token', result.data.token);
+      } else {
+        utils.quickTip('网络错误，请稍后再试');
+      }
+    }).catch((err) => {
+      console.log(err);
+    })
+  },
+  /**
+   * 在微信获取用户信息后进行用户信息初始化
+   */
+  initUserInfo: function (userInfo) {
     if (userInfo) {
       var that = this;
       // 调用login获取code，通过code获取openid，通过openid和当前用户做绑定
@@ -86,71 +136,27 @@ Page({
             }).then((res) => {
               let data = res.data;
               if (data.status == 200) {
-                let userInfo = data.data;
-                if (userInfo.userId) {
+                let yandaUser = data.data;
+                if (yandaUser.userId) {
                   // 用户已存在，用该用户在后台登录
-                  console.log('用户ID=' + data.data.userId);
-                  that.login(userInfo.userName, userInfo.password);
+                  console.log('用户ID=' + yandaUser.userId);
+                  that.login(yandaUser.userName, yandaUser.password);
                 } else {
                   // 该微信账号未在yanda注册账号，为其自动生成账号，并绑定openid,然后在yanda登录
-                  that.register(that.data.openid, userInfo.nickName, userInfo.avatarUrl);
+                  that.register(that.data.openid, userInfo.nickName, userInfo.avatarUrl, userInfo.gender);
                 }
               }
             });
           })
         }
       });
-
-
       app.globalData.userInfo = userInfo;
       this.setData({
         userInfo: userInfo,
         hasUserInfo: true
       });
+    } else {
+      utils.quickTip('无法获取用户信息');
     }
-  },
-
-  register: function (openId, nickName, avatar) {
-    $.post({
-      url: 'https://www.yanda123.com/yanda/user/registerByWechat',
-      header: { "Content-Type": "application/x-www-form-urlencoded" },
-      data: {
-        openId: openId,
-        nickName: nickName,
-        avatar: avatar
-      }
-    }).then((res) => {
-      let data = res.data;
-      if (data.status == 200) {
-        let userInfo = data.data;
-        console.log(userInfo);
-        that.login(userInfo.userName, userInfo.passowrd);
-      } else {
-        console.log('注册微信用户失败:' + data.message);
-      }
-    })
-  },
-  login: function (userName, password) {
-    $.post({
-      url: 'https://www.yanda123.com/yanda/user/login',
-      header: { "Content-Type": "application/x-www-form-urlencoded" },
-      data: {
-        userName: userName,
-        password: password
-      }
-    }).then((res) => {
-      let result = res.data;
-      if (result.status === -1) {
-        utils.quickTip(result.message);
-      } else if (result.status === 200) {
-        wx.setStorageSync('userInfo', result.data.userInfo);
-        wx.setStorageSync('sessionId', result.data.sessionId);
-        wx.setStorageSync('token', result.data.token);
-      } else {
-        util.quickTip('网络错误，请稍后再试');
-      }
-    }).catch((err) => {
-      console.log(err);
-    })
   }
 })
