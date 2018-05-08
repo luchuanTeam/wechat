04161432aceec3f227227000e0fb2b746850b485
melvzz <277514786@qq.com>
2018-05-08 17:11:25 +0800

    增加左滑删除功能

---
+++ pages/components/slider/slider.js
@@ -0,0 +1,77 @@
+var slide = {},
+  start = {};
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    sliderId: {
+      type: Number,
+      value: -1
+    },
+    initSliderId: {
+      type: Number,
+      observer(newVal, oldVal) {
+        if(newVal === this.properties.sliderId) {
+          this.init();
+        }
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    hideContentClass: '',
+    showDeleteClass: '',
+    deleted: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    touchStart(e) {
+      start = {   // 记录当前按下的位置
+        x: e.touches[0].pageX,
+        y: e.touches[0].pageY
+      }
+      this.triggerEvent('pressStart', { sliderId: this.properties.sliderId})
+    },
+
+    touchMove(e) {
+      slide = {
+        x: e.touches[0].pageX - start.x,
+        y: e.touches[0].pageY - start.y
+      }
+    },
+
+    touchEnd(e) {
+      this.triggerEvent('pressEnd');
+      if (slide.x < -50) {
+        this.setData({
+          hideContentClass: `left: -100rpx`,
+          showDeleteClass: `right: 0`
+        });
+        this.triggerEvent('slideLeft', { sliderId: this.properties.sliderId });
+      } else {
+        this.init();  
+      }
+      slide = {};
+    },
+
+    deleteSlider() {
+      this.setData({
+        deleted: true
+      })
+    },
+    
+    init() {
+      this.setData({
+        hideContentClass: '',
+        showDeleteClass: ''
+      })
+    }
+  }
+})
--- pages/user/collect/collect.js
+++ pages/user/collect/collect.js
@@ -1,12 +1,50 @@
-// pages/user/collect/collect.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    slides: [1,2,3],
+    currentPressId: null,
+    lastPressId: null,
+    initSliderId: null
   },
+
+  /**
+   * 触摸开始，子组件触发，记录下当前所要滑动的滑块
+   */
+  pressStart(e) {
+    this.setData({
+      currentPressId: e.detail.sliderId
+    })
+  },
+
+  /**
+   * 滑动结束，子组件触发，判断滑动结束后，当前要左滑的滑块是否和上次左滑的滑块一样，
+   * 如果不一样，则要将上次左滑的滑块恢复起始状态
+   */
+  pressEnd(e) {
+    let _lastPressId = this.data.lastPressId,
+        _currentPressId = this.data.currentPressId;
+    if(_lastPressId && _currentPressId !== _lastPressId) {
+      this.setData({
+        initSliderId: _lastPressId,
+        lastPressId: -1
+      })
+    } 
+  },
+
+  /**
+   * 已经发生了左滑，则要记录当前发生左滑的滑块，为下次滑动准备
+   */
+  slideLeft(e) {
+    let sliderId = e.detail.sliderId;
+    this.setData({
+      lastPressId: sliderId  
+    });
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
